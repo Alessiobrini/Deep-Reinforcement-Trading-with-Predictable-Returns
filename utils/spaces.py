Fix in advantage computation + asymm action space

utils/spaces.py
@@ -1,6 +1,7 @@
 from gin.config import configurable
 from gym.spaces.space import Space
 import numpy as np
+import pdb
 import gin
 gin.enter_interactive_mode()
 
@@ -28,15 +29,25 @@ class ActionSpace(Space):
         self, action_range: list, zero_action: bool = True, side_only: bool = False
     ):
         if not side_only:
-            self.values = np.round(
-                np.linspace(-action_range[0], action_range[0], action_range[1]), 2
-            )
+            if isinstance(action_range[0],list):
+                self.values = np.round(
+                    np.linspace(-action_range[0][0], action_range[0][1], action_range[1]), 2
+                )
+            else:
+                self.values = np.round(
+                    np.linspace(-action_range[0], action_range[0], action_range[1]), 2
+                )
         else:
             self.values = np.array([-1.0, 0.0, 1.0])
         if not zero_action:
             self.values = self.values[self.values != 0.0]
 
-        self.action_range = action_range
+        if isinstance(action_range[0],list):
+            self.action_range = action_range[0]
+            self.asymmetric = True
+        else:
+            self.action_range = action_range
+            self.asymmetric = False
         self.zero_action = zero_action
         self.side_only = side_only
         super().__init__(self.values.shape, self.values.dtype)

utils/test.py
@@ -100,7 +100,7 @@ class Out_sample_vs_gp:
 
                 gin.query_parameter("%ACTION_RANGE")[0] = action_range
                 test_agent.action_space = ActionSpace()
-
+            
             self.test_env = self.env_cls(
                 N_train=self.N_test,
                 f_speed=data_handler.f_speed,
@@ -162,9 +162,15 @@ class Out_sample_vs_gp:
                                 test_agent.action_space.action_range[0],test_agent.action_space.action_range[1], action
                             )
                         else:
-                            action = unscale_action(
-                                test_agent.action_space.values[-1], action
-                            )
+                            if test_agent.action_space.asymmetric:
+                                action = unscale_asymmetric_action(
+                                    test_agent.action_space.action_range[0],test_agent.action_space.action_range[1], action
+                                )
+                            else:
+                                action = unscale_action(
+                                    test_agent.action_space.action_range[0], action
+                                )
+
 
                     elif test_agent.policy_type == "discrete":
                         # action = test_agent.action_space.values[dist.sample()]
@@ -201,7 +207,7 @@ class Out_sample_vs_gp:
                 self.test_env.store_results(OptResult, i)
 
                 CurrOptState = NextOptState
-
+            # pdb.set_trace()
             if return_output:
                 return self.test_env.res_df
 
@@ -383,6 +389,7 @@ class Out_sample_vs_gp:
         if abs_wealthrl:
             self.abs_series_wealth_rl.loc[0, str(it)] = np.mean(abs_wealthrl)
             self.abs_series_wealth_gp.loc[0, str(it)] = np.mean(abs_wealthgp)
+        
 
 
     def save_series(self):

utils/tools.py
@@ -110,7 +110,7 @@ def get_action_boundaries(
         factors,
     )
 
-    if action_type == "GP" or action_type == "GPext":
+    if action_type == "GP" or action_type == "GPext" or action_type == "GPasym" or action_type == "GPsign":
         CurrOptState = env.opt_reset()
         OptRate, DiscFactorLoads = env.opt_trading_rate_disc_loads()
 
@@ -123,15 +123,46 @@ def get_action_boundaries(
             CurrOptState = NextOptState
 
         action_quantiles = env.res_df["OptNextAction"].quantile(qts[:2]).values
+        action_quantiles_correct = action_quantiles.copy()
+        action_quantiles_gp = action_quantiles.copy()
+
+        # #TODO temp code
+        # CurrMVState = env.opt_reset()
+        # cycle_len = len(returns) - 1
+        # for i in tqdm(iterable=range(cycle_len), desc="Selecting Action boundaries", disable=disable):
+        #     NextMVState, MVResult = env.mv_step(CurrMVState, i)
+        #     env.store_results(MVResult, i)
+        #     CurrMVState = NextMVState
+
+        # action_quantiles_mv = env.res_df["MVNextAction"].quantile(qts[:2]).values
+        # # TODO temp code
+
 
         if action_type == "GPext":
             action_quantiles[0] = action_quantiles[0] - action_quantiles[0]*qts[2]
             action_quantiles[1] = action_quantiles[1] + action_quantiles[1]*qts[2]
-    elif action_type == "MV":
+        
+            # action_quantiles_correct[0] = action_quantiles_correct[0] + action_quantiles_correct[0]*qts[2]
+            # action_quantiles_correct[1] = action_quantiles_correct[1] + action_quantiles_correct[1]*qts[2]
+            # print('Initial returns',env.returns[0])
+            # print('Asym qts', action_quantiles)
+            # print('Sym qts',action_quantiles_correct)
+            # print('GP qts', action_quantiles_gp)
+            # print('GP qts', action_quantiles_mv)
+            # print('GP pct of MV',100 - ((max(np.abs(action_quantiles_mv))-max(np.abs(action_quantiles_gp)))/max(np.abs(action_quantiles_gp))*100))
+            # pdb.set_trace()
+
+        elif action_type == "GPsign":
+            if np.sign(returns[0])<0.0:
+                action_quantiles[1] = 0.0
+            else:
+                action_quantiles[0] = 0.0
+
+    elif action_type == "MV" or action_type == "MVasym" or action_type == "MVmax":
         CurrMVState = env.opt_reset()
 
         cycle_len = len(returns) - 1
-        for i in tqdm(iterable=range(cycle_len), desc="Selecting Action boundaries"):
+        for i in tqdm(iterable=range(cycle_len), desc="Selecting Action boundaries", disable=disable):
             NextMVState, MVResult = env.mv_step(CurrMVState, i)
             env.store_results(MVResult, i)
             CurrMVState = NextMVState
@@ -142,8 +173,10 @@ def get_action_boundaries(
         print("Choose proper action type. Please, read the doc.")
         sys.exit()
 
-    if action_type == "GPext":
+    if action_type == "GPext" or action_type == "MVmax":
         action_range = np.max(np.abs(action_quantiles))
+    elif action_type == "GPasym" or action_type == "GPsign" or action_type == "MVasym":
+        action_range = list(action_quantiles)
     else:
         qt = np.min(np.abs(action_quantiles))
         length = len(str(int(np.round(qt))))
@@ -151,9 +184,9 @@ def get_action_boundaries(
 
     ret_range = float(max(np.abs(returns.min()), returns.max()))
 
-    if action_type == "GP" or action_type == "GPext":
+    if action_type == "GP" or action_type == "GPext" or action_type == "GPasym" or action_type == "GPsign":
         holding_quantiles = env.res_df["OptNextHolding"].quantile(qts[:2]).values
-    elif action_type == "MV":
+    elif action_type == "MV" or action_type == "MVasym" or action_type == "MVmax":
         holding_quantiles = env.res_df["MVNextHolding"].quantile(qts[:2]).values
 
     if np.abs(holding_quantiles[0]) - np.abs(holding_quantiles[1]) < 1000: